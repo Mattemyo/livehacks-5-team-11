51d2e0c07fd85e6b7d84411ef9d5fd053b90c1b5
Harald Bergqvist <hbergqvist@solidsport.com>
2018-11-24 14:10:18 +0100

    testing

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, () => console.log('Livehacks #4 team 7 listening on port 3000!'));
+app.listen(3000, () => console.log('Livehacks #5 team 11 listening on port 3000!'));
