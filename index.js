52f19d00cb9ff17a3afed4222413221306e8fab6
Mattemyo <matias97marti@gmail.com>
2018-11-25 14:22:25 +0100

    :art: set up websocket

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-const express = require('express');
-const app = express();
+
+var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
 
 app.use(express.static('public'));
 
 app.listen(3000, () => console.log('Livehacks #5 team 11 listening on port 3000!'));
+
+app.use((req, res, next) => {
+  console.log('middleware');
+  req.testing = 'testing';
+  return next();
+});
+
+app.get('/', (req, res, next) => {
+  console.log('get route', req.testing);
+  res.end();
+});
+
+app.ws('/:gameId', (ws, req) => {
+  ws.on('message', (msg) => {
+    console.log(msg);
+  });
+  console.log('socket', req.testing);
+});
--- public/src/main.ts
+++ public/src/main.ts
@@ -1,7 +1,7 @@
 const enum STATE {
-    PRE = 100,
-    RUNNING,
-    FINISHED
+  PRE = 100,
+  RUNNING,
+  FINISHED,
 }
 
 let gameState = STATE.PRE;
@@ -99,19 +99,19 @@ let users: User[] = [];
 let teams: Team[] = [];
 
 const registerUser = (message) => {
-    users.push({ uid: message.id, team: message.team, clicks: 0, isActive: true});
+  users.push({ uid: message.id, team: message.team, clicks: 0, isActive: true });
 
-    if (teams.filter((el) => el.tid === message.team).length === 0) {
-        teams.push({ tid: message.team, users: [], progress: 0.0, team_clicks: 0 });
-    }
+  if (teams.filter((el) => el.tid === message.team).length === 0) {
+    teams.push({ tid: message.team, users: [], progress: 0.0, team_clicks: 0 });
+  }
 
-    let index = teams.indexOf(teams.filter((el) => el.tid === message.team)[0]);
-    teams[index].users.push({ uid: message.id, team: message.team, clicks: 0, isActive: true});
+  let index = teams.indexOf(teams.filter((el) => el.tid === message.team)[0]);
+  teams[index].users.push({ uid: message.id, team: message.team, clicks: 0, isActive: true });
 };
 
 const createMessageHandler = () => {
   const removeInactiveUsers = () => {
-    if(gameState !== STATE.RUNNING){
+    if (gameState !== STATE.RUNNING) {
       return;
     }
     console.log('removed inactive users');
@@ -126,14 +126,13 @@ const createMessageHandler = () => {
   setInterval(removeInactiveUsers, 5000);
 
   return (message) => {
-    if(gameState === STATE.PRE){
-        return;
-    }else if(gameState === STATE.RUNNING){
-
-    let index;
-    if (users.filter((user) => user.uid === message.id).length === 0) {
-      registerUser(message);
-    } else {
+    if (gameState === STATE.PRE) {
+      return;
+    } else if (gameState === STATE.RUNNING) {
+      let index;
+      if (users.filter((user) => user.uid === message.id).length === 0) {
+        registerUser(message);
+      } else {
         const user: User = users.filter((user) => user.uid === message.id)[0];
         const team: Team = teams.filter((team) => team.tid === message.team)[0];
 
@@ -143,19 +142,19 @@ const createMessageHandler = () => {
         teams[index].users.filter((user) => user.uid === message.id)[0].isActive = true;
         team.team_clicks++;
 
-      if (team.users.length !== 0) {
-        team.progress = team.team_clicks / team.users.length / 2 / 100;
-      }
+        if (team.users.length !== 0) {
+          team.progress = team.team_clicks / team.users.length / 2 / 100;
+        }
 
-      if (team.progress >= 0.99) {
-        teams.sort((a, b) => {
-          return b.progress - a.progress;
-        });
+        if (team.progress >= 0.99) {
+          teams.sort((a, b) => {
+            return b.progress - a.progress;
+          });
           localStorage.startedAt = gameStartedAt;
-        localStorage.result = JSON.stringify(teams);
-        gameState = STATE.FINISHED;
-        window.location.href = '/scoreboard.html';
-      }
+          localStorage.result = JSON.stringify(teams);
+          gameState = STATE.FINISHED;
+          window.location.href = '/scoreboard.html';
+        }
 
         console.log(team.progress);
         updateTeamProgress(team.tid, team.progress);
@@ -172,7 +171,7 @@ const updateTeamProgress = (id, progress) => {
 };
 
 const roundToTwo = (val) => {
-    return Math.round(val * 100) / 100;
+  return Math.round(val * 100) / 100;
 };
 
 // ======== SCOREBOARD ======= //
@@ -183,13 +182,17 @@ const loserDivs = [].slice.call(document.getElementsByClassName('loser'));
 
 const onCompleted = (score, startedAt) => {
   const [winner, ...losers] = score;
-  const TPS = roundToTwo(winner.team_clicks / winner.users.length / (Math.floor(Date.now() / 1000) - startedAt));
+  const TPS = roundToTwo(
+    winner.team_clicks / winner.users.length / (Math.floor(Date.now() / 1000) - startedAt)
+  );
   winnerDiv.getElementsByTagName('img')[0].src = `/img/${teamColors[winner.tid]}-win.png`;
   winnerDiv.getElementsByTagName('p')[0].textContent = `${isNaN(TPS) ? 0 : TPS} TPS`;
 
   losers.forEach((loser, idx) => {
     const element = loserDivs[idx];
-    const TPS = roundToTwo(loser.team_clicks / loser.users.length / (Math.floor(Date.now() / 1000) - startedAt));
+    const TPS = roundToTwo(
+      loser.team_clicks / loser.users.length / (Math.floor(Date.now() / 1000) - startedAt)
+    );
     element.getElementsByTagName('img')[0].src = `/img/${teamColors[loser.tid]}-lose.png`;
     element.getElementsByTagName('p')[0].textContent = `${isNaN(TPS) ? 0 : TPS} TPS`;
   });
@@ -197,19 +200,25 @@ const onCompleted = (score, startedAt) => {
 
 let timeUntilStart = 5;
 const countdown = () => {
-  if(timeUntilStart > 0){
-    if(document.getElementById('countdown-label') === null){ return; }
+  if (timeUntilStart > 0) {
+    if (document.getElementById('countdown-label') === null) {
+      return;
+    }
     document.getElementById('countdown-label').textContent = timeUntilStart.toString();
     timeUntilStart -= 1;
     setTimeout(this.countdown, 1000);
-  }else{
-      document.getElementById('countdown-label').textContent = '';
+  } else {
+    document.getElementById('countdown-label').textContent = '';
     startGame();
   }
 };
 
 // ======  WEBSOCKET ======= //
-const WEBSOCKET_URL = 'ws://stagecast.se/api/events/livehacks_team11/ws?x-user-listener=1';
-const socket = createSocket(WEBSOCKET_URL);
+const IP_ADDRESS = '192.168.22.1';
+const PORT = 3000;
+const GAME_ID = 0;
+const WEBSOCKET_URL = `ws://${IP_ADDRESS}:${PORT}/${GAME_ID}/ws`;
+
+const socket = createSocket(`${WEBSOCKET_URL}?x-user-listener=1`);
 socket.setMessageHandler(createMessageHandler());
 countdown();
