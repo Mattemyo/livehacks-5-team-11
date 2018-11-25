bbc947fb3a34e4d35267b26990c290fa4444e0b2
Mattemyo <matias97marti@gmail.com>
2018-11-25 17:49:35 +0100

    :v: clean up and handle socket connections

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
@@ -7,20 +6,42 @@ app.use(express.static('public'));
 
 app.listen(3000, () => console.log('Livehacks #5 team 11 listening on port 3000!'));
 
-app.use((req, res, next) => {
-  console.log('middleware');
-  req.testing = 'testing';
-  return next();
-});
-
 app.get('/', (req, res, next) => {
   console.log('get route', req.testing);
   res.end();
 });
 
-app.ws('/:gameId', (ws, req) => {
-  ws.on('message', (msg) => {
+let userSocket, displaySocket;
+
+const displayHandler = (req) => {
+  displaySocket.on('message', (msg) => {
+    try {
+    } catch (error) {
+      console.error(error);
+    }
     console.log(msg);
   });
-  console.log('socket', req.testing);
-});
+};
+
+const userHandler = (req) => {
+  userSocket.on('message', (msg) => {
+    try {
+      displaySocket.send(msg);
+    } catch (error) {
+      console.error(error);
+    }
+    console.log(msg);
+  });
+};
+
+const middleware = (ws, req) => {
+  if (req.query['x-user-listener'] === '1') {
+    displaySocket = ws;
+    displayHandler(req);
+  } else {
+    userSocket = ws;
+    userHandler(req);
+  }
+};
+
+app.ws('/:gameId/ws', middleware);
